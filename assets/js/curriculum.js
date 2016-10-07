381c88249991e1a12e7f9ff847cb7769520c2c2e
Takahiro Kosoegawa <info@dscd.jp>
2016-10-07 16:49:49 +0900

    スクールライフ > 授業について

--- assets/js/curriculum.js
+++ assets/js/curriculum.js
@@ -1,77 +1,12 @@
 ///////////////////////////////////////////
 // 動画埋め込み
 ///////////////////////////////////////////
-var ytWidth = 640;
-var ytHeight = 390;
-
-// 授業風景
-var ytData1 = [
-    {
-        id: 'oryQkZ_RiYg'
-    }, {
-        id: 'yZ4MGBrN8EA'
-    }, {
-        id: 'XjbTH4LmHv8'
-    }, {
-        id: '4tokA2e8IFk'
-    }
-];
-
-// 特別授業
-var ytData2 = [
-    {
-        id: 'gfshwMI4jRY'
-    }, {
-        id: '8yraP942Fck'
-    }, {
-        id: 'IY71Vvxu2Ig'
-    }
-];
-
-var ytBaseUrl = 'https://www.youtube.com/embed/';
-
-// サムネイル画像の埋め込み準備
-var thumbInsert1 = '';
-for(var i = 0; i < ytData1.length; i++) {
-    thumbInsert1 += '<li><a href="' + ytBaseUrl + ytData1[i]['id'] + '">';
-    thumbInsert1 += '<img src="http://img.youtube.com/vi/' + ytData1[i]['id'] + '/mqdefault.jpg" alt="" />';
-    thumbInsert1 += '</a></li>';
+function changeFramePage0(pageUrl) {
+    frames[0].location.href = pageUrl;
 }
 
-
-var thumbInsert2 = '';
-for(var i = 0; i < ytData2.length; i++) {
-    thumbInsert2 += '<li><a href="' + ytBaseUrl + ytData2[i]['id'] + '">';
-    thumbInsert2 += '<img src="http://img.youtube.com/vi/' + ytData2[i]['id'] + '/mqdefault.jpg" alt="" />';
-    thumbInsert2 += '</a></li>';
-}
-
-var ytPlayer1, ytPlayer2;
-// YouTube Player APIを読み込む
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// API読み込み後にプレーヤーを埋め込む
-function onYouTubeIframeAPIReady() {
-    ytPlayer1 = new YT.Player('player1', {
-        height: ytHeight,
-        width: ytWidth,
-        videoId: ytData1[0]['id']
-    });
-    ytPlayer2 = new YT.Player('player2', {
-        height: ytHeight,
-        width: ytWidth,
-        videoId: ytData2[0]['id']
-    });
-}
-// 動画の再生
-function play1(ytId, seekTime) {
-    ytPlayer1.loadVideoById(ytId1, seekTime);
-}
-function play2(ytId, seekTime) {
-    ytPlayer2.loadVideoById(ytId2, seekTime);
+function changeFramePage1(pageUrl) {
+    frames[1].location.href = pageUrl;
 }
 
 
